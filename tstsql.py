3eed5e822692c82d597f15c2fdd097e442c0fa8e
DarkG <g3176703106@163.com>
2026-06-03 11:08:46 +0800

    20260603m

--- tstsql.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-
-conn=pymysql.connect(host='localhost',port=3306,user='root',password='123456',database='test')
-cursor=conn.cursor()
-cursor.execute("show tables")
-print(cursor.fetchall())
-cursor.close()
-conn.close()
